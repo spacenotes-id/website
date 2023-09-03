dc78b7fc45fd8a0ad246797106b3d1a1ea42a66b
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2023-09-03 21:42:01 +0700

    feat(header): create breadcrumb

---
+++ src/components/header/breadcrumb.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export function Breadcrumb() {
+  const pathname = usePathname()
+
+  const segments = pathname.split('/').filter((segment) => segment !== '') // Split and remove empty segments
+
+  const items = segments.map((segment, index) => {
+    const path = '/' + segments.slice(0, index + 1).join('/') // Build the path for each breadcrumb
+    const name = segment.charAt(0).toUpperCase() + segment.slice(1) // Capitalize the name
+
+    return {
+      href: path,
+      name: name,
+      isLastElement: index === segments.length - 1,
+    }
+  })
+
+  return (
+    <nav className='inline-flex items-center'>
+      {items.map((item) => {
+        if (item.isLastElement)
+          return (
+            <span className='text-sm font-medium' key={item.href}>
+              {item.name}
+            </span>
+          )
+        return (
+          <span key={item.href} className='inline-flex items-center mr-1.5'>
+            <Link
+              className='text-sm font-medium motion-safe:transition motion-safe:hover:bg-primary-300'
+              href={item.href}
+            >
+              <span>{item.name}</span>
+            </Link>
+
+            <span className='ml-1'>/</span>
+          </span>
+        )
+      })}
+    </nav>
+  )
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import { Paper } from '@/components/paper'
 
+import { Breadcrumb } from './breadcrumb'
 import { Navbar } from './navbar'
 
 export function Header() {
@@ -7,6 +8,7 @@ export function Header() {
     <div className='fixed top-0 inset-x-0'>
       <Paper as='header' className='h-14 sm:pl-52 md:pl-60 xl:pl-72'>
         <div className='flex items-center h-full w-11/12 mx-auto'>
+          <Breadcrumb />
           <Navbar />
         </div>
       </Paper>
