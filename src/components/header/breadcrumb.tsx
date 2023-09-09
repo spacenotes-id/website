8585bf4db9be55094a2b10265922fb528de98789
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2023-09-10 05:43:30 +0700

    fix(breadcrumb): show dashboard icon if it has only one item

--- src/components/header/breadcrumb.tsx
+++ src/components/header/breadcrumb.tsx
@@ -2,6 +2,7 @@ import { tw } from '@/libs/common'
 
 import { LayoutDashboardIcon, ChevronRightIcon } from 'lucide-react'
 import Link from 'next/link'
+import { match } from 'ts-pattern'
 
 export type TBreadcrumb = { label: string; path: string }
 
@@ -20,14 +21,22 @@ export function Breadcrumb(props: TProps) {
         if (lastElement)
           return (
             <span className='inline-flex items-center py-0.5' key={item.path}>
-              <ChevronRightIcon className='shrink-0' size='0.95em' />
-              <span className='text-xs sm:text-sm font-medium'>{item.label}</span>
+              {match(self.length)
+                .with(1, () => <LayoutDashboardIcon className='shrink-0' size='0.95em' />)
+                .otherwise(() => (
+                  <ChevronRightIcon className='shrink-0' size='0.95em' />
+                ))}
+              <span className='ml-1 text-xs sm:text-sm font-medium'>{item.label}</span>
             </span>
           )
 
         return (
           <span key={item.path} className='inline-flex items-center'>
-            {index !== 0 && <ChevronRightIcon className='shrink-0' size='0.95em' />}
+            {match(index)
+              .with(0, () => null)
+              .otherwise(() => (
+                <ChevronRightIcon className='shrink-0' size='0.95em' />
+              ))}
             <Link
               className={tw(
                 'inline-flex items-center',
@@ -38,7 +47,9 @@ export function Breadcrumb(props: TProps) {
               )}
               href={item.path}
             >
-              {index === 0 && <LayoutDashboardIcon size='0.95em' className='mr-1 shrink-0' />}
+              {match(index)
+                .with(0, () => <LayoutDashboardIcon size='0.95em' className='mr-1 shrink-0' />)
+                .otherwise(() => null)}
               <span className='shrink-0'>{item.label}</span>
             </Link>
           </span>
